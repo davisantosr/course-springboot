a8980d8f8dfd6f9e740bec38d207b703ed6187dc
DAVI SANTOS <davi-rocha2@hotmail.com>
2020-08-02 16:58:37 -0300

    Project created

---
+++ src/test/java/com/github/davisantosr/coursewebservices/CourseWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.davisantosr.coursewebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
