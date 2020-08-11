6b7929a0389e56d58a72925047ce3bbed579fc99
DAVI SANTOS <davi-rocha2@hotmail.com>
2020-08-11 11:23:24 -0300

    Payment, one-to-one association

--- src/main/java/com/github/davisantosr/coursewebservices/config/TestConfig.java
+++ src/main/java/com/github/davisantosr/coursewebservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.github.davisantosr.coursewebservices.entities.Category;
 import com.github.davisantosr.coursewebservices.entities.Order;
 import com.github.davisantosr.coursewebservices.entities.OrderItem;
+import com.github.davisantosr.coursewebservices.entities.Payment;
 import com.github.davisantosr.coursewebservices.entities.Product;
 import com.github.davisantosr.coursewebservices.entities.User;
 import com.github.davisantosr.coursewebservices.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 			
 	}
